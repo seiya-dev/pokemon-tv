c667cfc084c1b9ae8a7d96bfd96f9e4f1dfa60bf
seiya-git <seiya.loveless.mail@gmail.com>
2022-11-19 13:59:09 +0300

    updater

---
+++ ptv-updater.js
@@ -0,0 +1,50 @@
+const gotCfg = {
+    headers: { 
+        'user-agent': [
+            'Mozilla/5.0',
+            '(Windows NT 10.0; Win64; x64; rv:70.0)',
+            'Gecko/20100101 Firefox/70.0',
+        ].join(' '),
+    },
+};
+
+const tvRegion = [
+    'us', 'uk', 'fr', 'it',
+    'de', 'es', 'el', 'br',
+    'ru', 'dk', 'nl', 'fi', 
+    'no', 'se',
+];
+
+const proxyCode = {
+    uk: 'gb',
+    el: 'mx',
+};
+
+
+import { execSync } from 'node:child_process';
+import * as dotenv from 'dotenv';
+import gm from 'got';
+
+dotenv.config();
+const user = process.env.PROXY_USER;
+const pass = process.env.PROXY_PASS;
+const got = gm.extend(gotCfg);
+
+try{
+    const proxyData = await got('https://assets.windscribe.com/serverlist/firefox/1/1?platform=firefox');
+    const proxyJson = JSON.parse(proxyData.body);
+    for(let tvr of tvRegion){
+        const cc = proxyCode[tvr] ? proxyCode[tvr] : tvr;
+        const px = proxyJson.data.find(i => {
+            return i.country_code.toLowerCase() == cc;
+        });
+        if(px){
+            const proxyHost = px.groups[0].hosts[0].hostname;
+            execSync(`node pokemon.js --proxy "https://${user}:${pass}@${proxyHost}/" --cc ${tvr}`, { stdio: 'inherit' });
+        }
+    }
+}
+catch(e){
+    console.log('[error] update failed!');
+    console.log(e);
+}
